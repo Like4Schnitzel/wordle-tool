d8ed693b19ec11757abead83cff67104350c1e95
Laura <rw21062006@gmail.com>
2025-05-12 23:11:11 +0200

    i mean like it works

---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+export type Letter = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
+export type Color = 'green' | 'yellow' | 'gray';
+
+export type Character = {
+    letter: Letter | null;
+    color: Color | null;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,225 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import words from '$lib/words.json';
+    import type { Character, Color, Letter } from '$lib/types.ts';
+
+    let filteredWords = $state(words);
+    const colors: Color[] = ['gray', 'yellow', 'green'];
+
+    const word: Character[] = $state((() => {
+        const tmp = [];
+        for (let i = 0; i < 5; i++) {
+            tmp.push({ letter: null, color: null });
+        }
+        return tmp;
+    })());
+    const characterInputs: HTMLInputElement[] = $state([]);
+
+    const previousWords: Character[][] = $state([]);
+    const wordFilters: {letter: Letter, color: Color, index: number}[] = $state([]);
+
+    function resetCurrentWord() {
+        for (const character of word) {
+            character.letter = null;
+            character.color = null;
+        }
+        for (const input of characterInputs) {
+            input.value = '';
+        }
+    }
+
+    function addWord() {
+        const enteredWord = word.map(character => character.letter).join('');
+        if (!words.includes(enteredWord)) {
+            alert('Invalid word.');
+            return;
+        }
+
+        const newWord: Character[] = [];
+        for (let i = 0; i < word.length; i++) {
+            const character = word[i];
+            newWord.push({ ...character });
+            wordFilters.push({ letter: character.letter!, color: character.color!, index: word.indexOf(character) });
+        }
+        previousWords.push(newWord);
+
+        resetCurrentWord();
+        applyFilter();
+    }
+
+    function handleInput(index: number) {
+        try {
+            let enteredLetter = characterInputs[index].value;
+            if (enteredLetter === null) return;
+
+            enteredLetter = enteredLetter.trim().toUpperCase();
+            if (enteredLetter < 'A' || enteredLetter > 'Z') {
+                characterInputs[index].value = word[index].letter ?? '';
+                return;
+            }
+
+            word[index].letter = enteredLetter as Letter;
+            characterInputs[index].value = word[index].letter;
+        } catch {
+            word[index].letter = null;
+            characterInputs[index].value = '';
+        }
+    }
+
+    function applyFilter() {
+        filteredWords = filteredWords.filter(word => {
+            // check there aren't any letters that shouldn't be there
+            for (const letter of word) {
+                const filters = wordFilters.filter(filter => filter.letter === letter);
+                for (const filter of filters) {
+                    if (filter.color === 'gray') {
+                        return false;
+                    }
+
+                    if (filter.color === 'yellow' && filter.index === word.indexOf(letter)) {
+                        return false;
+                    }
+
+                    if (filter.color === 'green' && filter.index !== word.indexOf(letter)) {
+                        return false;
+                    }
+                }
+            }
+
+            // check all letters that should be there are there
+            for (const filter of wordFilters.filter(filter => filter.color !== 'gray')) {
+                if (!word.includes(filter.letter)) {
+                    return false;
+                }
+
+                if (filter.color === 'green' && filter.index !== word.indexOf(filter.letter)) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        console.log('Filtered words:', filteredWords);
+    }
+</script>
+
+<div class="main">
+    <h1>Enter a word and the response you got.</h1>
+    <form onsubmit={addWord}>
+        <div class="word">
+            {#each word as letter, index}
+                <div>
+                    <input type="text" oninput={() => handleInput(index)} bind:this={characterInputs[index]} maxlength="1" autocomplete="off" required />
+                    <select bind:value={word[index].color} required>
+                        {#each colors as color}
+                            <option value={color}>{color}</option>
+                        {/each}
+                </div>
+            {/each}
+        </div>
+        <button type="submit">Add Word</button>
+    </form>
+    <div class="previous-words">
+        <h1>Previous Words</h1>
+        <div class="words">
+            {#each previousWords as word, index}
+                <div class="previous-word">
+                    {#each word as letter}
+                        <span class="{letter.color}">{letter.letter}</span>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+    </div>
+    <div class="possible-words">
+        <h1>Possible Words</h1>
+        <div class="words">
+            {#each filteredWords as word}
+                <span>{word}</span>
+            {/each}
+        </div>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        background-color: #121212;
+    }
+    
+    .main {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: center;
+        margin-top: 2rem;
+        font-family: monospace;
+        color: white;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: center;
+    }
+
+    .word {
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+        justify-content: center;
+    }
+    
+    .word div {
+        display: flex;
+        flex-direction: column;
+        gap: 0.4rem;
+    }
+
+    div input {
+        width: 1rem;
+        text-align: center;
+    }
+
+    div select {
+        width: 1.5rem;
+        text-align: center;
+    }
+
+    .words {
+        display: flex;
+        gap: 0.5rem;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .previous-word {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+    }
+
+    .previous-word span {
+        font-size: 1rem;
+        width: 1rem;
+        height: 1rem;
+        text-align: center;
+        outline: 0.1rem solid white;
+    }
+
+    .gray {
+        background-color: #3a3a3c;
+    }
+
+    .yellow {
+        background-color: #b59f3b;
+    }
+
+    .green {
+        background-color: #538d4e;
+    }
+</style>
